9227cfa8840703ffc58099e36d67252b6ec823d8
Davidodari <davidkibzodari@gmail.com>
2018-12-11 18:09:47 +0300

    chore :  Added Async Task For Retrieving Jokes from endpoint

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/NetworkUtils/EndpointsAsyncTask.java
@@ -0,0 +1,48 @@
+package com.udacity.gradle.builditbigger.NetworkUtils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Pair;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... pairs) {
+        if (myApiService == null) {
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                            request.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+        context = pairs[0].first;
+        String name = pairs[0].second;
+        try {
+            return myApiService.provideJoke().execute().getData();
+        }catch (IOException e){
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+//        Launch Intent
+        Toast.makeText(context,result,Toast.LENGTH_LONG).show();
+    }
+}
