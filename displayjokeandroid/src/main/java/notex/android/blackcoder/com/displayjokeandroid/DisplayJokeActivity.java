8508e29e549891b961deb30439d89ea2ae3f9329
Davidodari <davidkibzodari@gmail.com>
2018-12-12 01:44:56 +0300

    refactor : Display Jokes in TextView in android library

--- displayjokeandroid/src/main/java/notex/android/blackcoder/com/displayjokeandroid/DisplayJokeActivity.java
+++ displayjokeandroid/src/main/java/notex/android/blackcoder/com/displayjokeandroid/DisplayJokeActivity.java
@@ -3,20 +3,22 @@ package notex.android.blackcoder.com.displayjokeandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_joke);
+        textView = findViewById(R.id.tv_joke);
 //        Get Intent
         String KEY_JOKE = "joke";
         Intent intent = getIntent();
         if (intent.hasExtra(KEY_JOKE)) {
 //            Display Joke if intent has joke
-            Toast.makeText(this, intent.getStringExtra(KEY_JOKE), Toast.LENGTH_LONG).show();
+            String joke = String.format("%s", intent.getStringExtra(KEY_JOKE));
+            textView.setText(joke);
         }
     }
 }
