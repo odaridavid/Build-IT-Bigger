dc4c9cabaf6ae97bacb455badaa66a217b2fea5f
Davidodari <davidkibzodari@gmail.com>
2018-12-12 02:17:40 +0300

    refactor : Add paid version activity and resources

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,45 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import notex.android.blackcoder.com.displayjokeandroid.DisplayJokeActivity;
+import notex.android.blackcoder.com.jokeproviderjava.JokeProvider;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        Button btnJoke = root.findViewById(R.id.btn_joke_launch);
+        btnJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tellJoke();
+            }
+        });
+        return root;
+    }
+
+    public void tellJoke() {
+        JokeProvider jokeProvider = new JokeProvider();
+        Intent intent = new Intent(getContext(), DisplayJokeActivity.class);
+        String KEY_JOKE = "joke";
+        intent.putExtra(KEY_JOKE, jokeProvider.jokeResponse());
+        startActivity(intent);
+    }
+
+}
