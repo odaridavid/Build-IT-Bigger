d160984d296833b3640e665eceadc5b2f0f4f5a2
Davidodari <davidkibzodari@gmail.com>
2018-12-12 01:43:54 +0300

    refactor : Add Interstitial add and listener to reload ads - Ads Display when navigating back to main activity

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragmentAds.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragmentAds.java
@@ -1,19 +1,19 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
-import com.udacity.gradle.builditbigger.NetworkUtils.EndpointsAsyncTask;
 
 import notex.android.blackcoder.com.displayjokeandroid.DisplayJokeActivity;
 import notex.android.blackcoder.com.jokeproviderjava.JokeProvider;
@@ -33,13 +33,34 @@ public class MainActivityFragmentAds extends Fragment {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 //        Initialize Ads - Sample App Id
         MobileAds.initialize(inflater.getContext(), "ca-app-pub-3940256099942544~3347511713");
+//        Interstitial Ad - Full Screen Ad
+        final InterstitialAd mInterstitialAd = new InterstitialAd(inflater.getContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+//        Find Views in Fragment Xml
         AdView mAdView = root.findViewById(R.id.adView);
         Button btnJoke = root.findViewById(R.id.btn_joke_launch);
         btnJoke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tellJoke();
+//                Display Interstiial Ad when navigating back from display activity
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                    tellJoke();
+                } else {
+                    Toast.makeText(getContext(), "Ad Not Loaded Yet", Toast.LENGTH_LONG).show();
+                    tellJoke();
+                }
             }
+        });
+//        Load new Ad
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
         });
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
